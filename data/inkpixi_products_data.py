29b3ed161aa41bb0dc511822d0af55871f90f488
dhoy <dhoy@earthsunmoon.com>
2017-09-26 14:11:02 -0400

    search
    added search bar and completor

--- data/inkpixi_products_data.py
+++ data/inkpixi_products_data.py
@@ -1,5 +1,7 @@
 import mysql.connector
 from contextlib import closing
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtGui import QIcon
 
 def connect_mysql():
     conn = mysql.connector.connect(user = 'root', password = 'rowsby01', host = 'APPSERVER1', database = 'inkpixi_art', raise_on_warnings = True) 
@@ -21,6 +23,24 @@ def get_companies():
     
     return lst_companies
 
+def get_sku_names(company_id):
+    con = connect_mysql()
+    cur = con.cursor()
+    lst_results = []
+    
+    try:
+        with closing(cur) as db:
+            db.callproc('get_skus_names', [company_id])
+            for result in db.stored_results():
+                results = result.fetchall()
+    except BaseException as e:
+        throw_db_error(e)
+        
+        
+    for result in results:
+        lst_results.append(result[0]) 
+    return lst_results 
+    
 def get_sku_info(sku, company_id):
     con = connect_mysql()
     cur = con.cursor()
@@ -44,5 +64,14 @@ def get_available_sizes_per_garm_color(garm_color):
         lst_results.append(result[0])
         
     return lst_results
-            
+
+        
+def throw_db_error(error):
+    ErrorMessage(error)
     
+    
+class ErrorMessage(QMessageBox):
+    def __init__(self, error):
+        super(ErrorMessage, self).__init__()
+        QMessageBox.critical(self, 'A database error has occured.', str(error))
+
